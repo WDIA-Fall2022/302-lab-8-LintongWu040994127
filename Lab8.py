13972832c6e71eb3e7bb23cc0ba323ecdb2ff524
Lintong Wu <95187457+LintongWu040994127@users.noreply.github.com>
2022-11-10 13:39:20 -0500

    Update Lab8.py

--- Lab8.py
+++ Lab8.py
@@ -8,14 +8,14 @@
 #Returns the number of Tests
 def getNumberOfTests(prompt):
     while True:
-    try:
-        tests = int(input("Please enter the number of tests: "))
-    except ValueError:
-        print("Please enter an integer.")
-        continue
-    else:
-        break
-   return tests
+        try:
+            tests = int(input("Please enter the number of tests: "))
+        except ValueError:
+            print("Please enter an integer.")
+            continue
+        else:
+            break
+        return tests
 
 #Prompts the user for the weigth of Assignments
 #Note that this function will include call(s) to the input function
@@ -23,59 +23,60 @@ def getNumberOfTests(prompt):
 #Returns the weight of assignments
 def getWeightOfAssignments(prompt):
     while True:
-    try:
-        wAssign = float(input("Please enter the weigth of Assignments: "))
-    except ValueError:
-        print("Please enter a float number between 0 and 1.")
-        return getWeightOfAssignments(prompt)
-   
-   if wAssign < 0 or wAssign > 1:
-        print("Please enter a float number between 0 and 1.")
-        return getWeightOfAssignments(prompt)
-    else:
-        return wAssign
+        try:
+            wAssign = float(input("Please enter the weigth of Assignments: "))
+        except ValueError:
+            print("Please enter a float number between 0 and 1.")
+            return getWeightOfAssignments(prompt)
+    
+        if wAssign < 0 or wAssign > 1:
+                print("Please enter a float number between 0 and 1.")
+                return getWeightOfAssignments(prompt)
+        else:
+            return wAssign
 
 #Prompts the user for the weigth of Midterms
 #Note that this function will include call(s) to the input function
 #Keep prompting until the number is a float >= 0 and <= 1
 #Returns the weight of midterms
-def getWeightOfMidTerms():
+def getWeightOfMidTerms(prompt):
     while True:
-    try:
-        wMidTerm = float(input("Please enter the weigth of midterms: "))
-    except ValueError:
-        print("Please enter a float number between 0 and 1.")
-        return getWeightOfMidTerms(prompt)
+        try:
+            wMidTerm = float(input("Please enter the weigth of midterms: "))
+        except ValueError:
+            print("Please enter a float number between 0 and 1.")
+            return getWeightOfMidTerms(prompt)
    
-   if wMidTerm < 0 or wMidTerm > 1:
-        print("Please enter a float number between 0 and 1.")
-        return getWeightOfMidTerms(prompt)
-    else:
-        return wMidTerm
+        if wMidTerm < 0 or wMidTerm > 1:
+                print("Please enter a float number between 0 and 1.")
+                return getWeightOfMidTerms(prompt)
+        else:
+            return wMidTerm
 
 #Prompts the user for the weigth of the final
 #Note that this function will include call(s) to the input function
 #Keep prompting until the number is a float >= 0 and <= 1
 #Returns the weight of final
-def getWeightOfFinal():
+def getWeightOfFinal(prompt):
     while True:
-    try:
-        wFinal = float(input("Please enter the weigth of final: "))
-    except ValueError:
-        print("Please enter a float number between 0 and 1.")
-        return getWeightOfFinal(prompt)
-   
-   if wFinal < 0 or wFinal > 1:
-        print("Please enter a float number between 0 and 1.")
-        return getWeightOfFinal(prompt)
-    else:
-        return wFinal
+        try:
+            wFinal = float(input("Please enter the weigth of final: "))
+        except ValueError:
+            print("Please enter a float number between 0 and 1.")
+            return getWeightOfFinal(prompt)
+    
+        if wFinal < 0 or wFinal > 1:
+            print("Please enter a float number between 0 and 1.")
+            return getWeightOfFinal(prompt)
+        else:
+            return wFinal
 
 #returns True if the sum of the 3 arguments is 1, False otherwise
 #Assign the default values 0.4 0.35 0.25 to wAssign, wMidtern and wFinal respectively
 def checkWeights(wAssign,wMidTerm,wFinal):
     sumOfThree = wAssign + wMidTerm + wFinal
-    if sumOfThree = 1:
+    
+    if sumOfThree == 1:
         return True
     else:
         return False
@@ -121,15 +122,15 @@ def calculateLetterGrade(numericGrade):
 #Get the weight value of the final (call the appropriate function)
 #Check the sum of weight values is 1 (call the appropriate function)
 #Repeat the last four lines if not equal to 1
-while(1):
-    wAssign = float(input("Please enter the weigth of Assignments: "))
-    wMidTerm = float(input("Please enter the weigth of midterms: "))
-    wFinal = float(input("Please enter the weigth of final: "))
+while 1:
+    getWeightOfAssignments(prompt)
+    getWeightOfMidTerms(prompt)
+    getWeightOfFinal(prompt)
     if wAssign + wMidTerm + wFinal == 1: 
         break 
 #Get the average grade obtained on the assignments
 #Validate the input as a float between 0 and 100
-while(1):
+while 1:
     AvgAssignments = float(input('Please enter the average grade of assignments: '))
     if AvgAssignments >= 0 and AvgAssignments <= 100:
         break 
@@ -137,10 +138,10 @@ while(1):
 #Prompt the user for each test grades and accumulate the value
 #Validate the input as a float between 0 and 100
 #Calculate the average test grade.
-numOfTests = int(input('Enter number of tests: '))
+getNumberOfTests(prompt)
 for i in range(n):
-    while(1):
-        test = float(input('Enter Test marks: '))
+    while 1:
+        test = float(input('Please enter test marks: '))
         if test >= 0 and test <= 100:
             i += k 
             break 
@@ -148,12 +149,13 @@ AvgTests = i/n
 
 #Prompt and get the final grade
 #Validate the input as a float between 0 and 100
-while(1):
-    final = float(input('Please enter the final grade: '))
+while 1:
+    getWeightOfFinal(prompt)
     if final >= 0 and final <= 100:
         break 
 #Calculate and display the final numeric grade (call the appropriate function)
 calculateNumericGrade(AvgAssignments,AvgTests,final,wAssign,wMidTerm,wFinal)
 #Calculate and display the final alphabetical grade (call the appropriate function)
 calculateLetterGrade(numericGrade)
+
 
